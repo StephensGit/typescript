e55af5cb3800628010cea3485cbdd7dc74dec6ee
StephensGit <stephenmoran@live.ie>
2021-02-27 14:33:52 +0000

    The DOM & Type Casting

--- public/app.js
+++ public/app.js
@@ -1,27 +1,7 @@
 "use strict";
-// Function Signatures
-// Example 1
-var greet;
-greet = function (name, greeting) {
-    console.log(name + " says " + greeting);
-};
-greet("Dave", "Hello");
-// Example 2
-var calc;
-calc = function (numOne, numTwo, action) {
-    if (action == "add") {
-        return numOne + numTwo;
-    }
-    else {
-        return numTwo - numOne;
-    }
-};
-var calcResult = calc(20, 30, "minus");
-console.log(calcResult);
-//  Example 3
-var logDetails;
-logDetails = function (person) {
-    console.log(person.name + " is " + person.age + " years old");
-};
-var stephen;
-var personLogDetails = logDetails(stephen);
+var anchor = document.querySelector('a');
+if (anchor) {
+    console.log(anchor.href);
+}
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,10 @@
-// Function Signatures
+const anchor = document.querySelector('a')!;
 
-// Example 1
-let greet: (a: string, b: string) => void;
-
-greet = (name: string, greeting: string) => {
-    console.log(`${name} says ${greeting}`);
+if(anchor) {
+    console.log(anchor.href);
 }
 
-greet("Dave", "Hello");
-
-// Example 2
-let calc: (a: number, b: number, c: string) => number;
-
-calc = (numOne: number, numTwo: number, action: string) => {
-    if(action == "add") {
-        return numOne + numTwo;
-    } else {
-        return numTwo - numOne;
-    }
-}
-
-let calcResult = calc(20, 30, "minus");
-console.log(calcResult);
-
-//  Example 3
-let logDetails: (obj: {name: string, age: number}) => void;
-
-logDetails = (person: {name:string, age:number}) => {
-    console.log(`${person.name} is ${person.age} years old`);
-};
-
-let stephen: object;
-let personLogDetails = logDetails(stephen);
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
 
 
