3032e3b41094c4c903415c0aaa3ec2084616857f
StephensGit <stephenmoran@live.ie>
2021-02-27 14:57:33 +0000

    Classes - ap

--- public/app.js
+++ public/app.js
@@ -1,4 +1,22 @@
 "use strict";
+//  Classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, b, a) {
+        this.client = c;
+        this.details = b;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " to " + this.details;
+    };
+    return Invoice;
+}());
+var inOne = new Invoice('David', 'Dylan', 200);
+var inTwo = new Invoice('Stephen', 'Gerard', 400);
+var invoices = [];
+invoices.push(inOne);
+invoices.push(inTwo);
+console.log(invoices);
 var form = document.querySelector('.new-item-form');
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
+//  Classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, b: string, a: number) {
+        this.client = c;
+        this.details = b;
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes ${this.amount} to ${this.details}`
+    }
+}
+
+const inOne = new Invoice('David', 'Dylan', 200);
+const inTwo = new Invoice('Stephen', 'Gerard', 400);
+
+let invoices: Invoice[] = [];
+invoices.push(inOne);
+invoices.push(inTwo);
+
+console.log(invoices);
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 const type = document.querySelector('#type') as HTMLFormElement;
